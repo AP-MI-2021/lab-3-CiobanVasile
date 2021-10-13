787899d4e49a394c795a99864e9f9d6cddf99156
Vasi <v.cioban@yahoo.com>
2021-10-13 11:25:17 +0300

    Am rezolvat proprietatile

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def arata_meniu():
     print('1. Citire lista')
     print('2. Afisare cea mai lunga subsecventa cu toate numerele prime')
     print('3. Afisare cea mai lunga subsecventa cu toate numerele care au cifrele prime')
+    print('4. Afisare cea mai lunga subsecventa cu toate numerele neprime')
     print('x. Quit')
 
 
@@ -70,6 +71,29 @@ def get_longest_all_primes(lst: list[int]) -> List[int]:
                     result = lst[st:dr+1]
     return result
 
+def get_longest_all_not_prime(lst: list[int]) -> List[int]:
+    '''
+    Determina cea mai lunga subsecventa in care toate numerel sunt neprime
+    Input:
+    -O lista de numere intregi, naturale
+    Output:
+    -Cea mai lunga subsecventa de numere care sunt neprime,returnata printr-o lista
+    '''
+    lungime = len(lst)
+    result = []
+    for st in range(lungime):
+        for dr in range(st, lungime):
+            all_non_prime = True
+            for numar in lst[st:dr + 1]:
+                if is_prime(numar) == True:
+                    all_non_prime = False
+                    break
+            if all_non_prime == True:
+                if dr - st + 1 > len(result):
+                    result = lst[st:dr + 1]
+    return result
+
+
 def get_longest_prime_digits(lst: list[int]) -> List[int]:
     '''
         Determina cea mai lunga subsecventa in care toate numerele au cifrele prime.
@@ -113,6 +137,10 @@ def test_get_longest_prime_digits():
     assert get_longest_prime_digits([44,44,22,22,22,66,55,33]) == [22,22,22]
     assert get_longest_prime_digits([333,333,444,222,222,555,888]) == [222,222,555]
 
+def test_get_longest_all_not_prime():
+    assert get_longest_all_not_prime([1,2,3,4,6])==[4,6]
+    assert get_longest_all_not_prime([2,2,2,4,4,2,2,2,4,4,4])==[4,4,4]
+    assert get_longest_all_not_prime([5,5,5,6,6,5,6,5,6,6,6,5,6,6,6,6])==[6,6,6,6]
 
 def test_is_digit_prime():
     assert is_digit_prime(2222) == True
@@ -128,9 +156,11 @@ def main():
         if option == '1':
             list = citire_list()
         elif option == '2':
-            print('Cea mai lunga subsecventa cu toate numerele divizibile cu 10 este:', get_longest_all_primes(list))
+            print('Cea mai lunga subsecventa cu toate numerele prime:', get_longest_all_primes(list))
         elif option == '3':
             print("Cea mai lunga subsecventa cu toate numerele care au cifrele prime este:", get_longest_prime_digits(list))
+        elif option == '4':
+            print("Cea mai lunga subsecventa cu toate numerele neprime:",get_longest_all_not_prime(list))
         elif option == 'x':
             break
         else:
@@ -140,6 +170,7 @@ def main():
 if __name__ == '__main__':
     test_get_longest_prime_digits()
     test_get_longest_all_primes()
+    test_get_longest_all_not_prime()
     test_is_prime()
     test_is_digit_prime()
     main()
